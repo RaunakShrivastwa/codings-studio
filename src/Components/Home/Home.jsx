7f616ab01f21b787abeb5b086962e8ebb6338920
Shubham Shrivastwa <kumarraunak4201@gmail.com>
2025-02-19 01:29:56 +0530

    Home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,52 +1,18 @@
 import React from 'react'
-
+import './Home.scss'
 const Home = () => {
   return (
-    <div>
-      <ul>
-        <li>list-1</li>
-        <li>list-2</li>
-        <li>list-3</li>
-        <li>list-4</li>
-        <li>list-5</li>
-        <li>list-6</li>
-        <li>list-7</li>
-        <li>list-8</li>
-        <li>list-9</li>
-        <li>list-10</li>
-        <li>list-11</li>
-        <li>list-12</li>
-        <li>list-13</li>
-        <li>list-14</li>
-        <li>list-15</li>
-        <li>list-16</li>
-        <li>list-17</li>
-        <li>list-18</li>
-        <li>list-19</li>
-        <li>list-20</li>
-        <li>list-21</li>
-        <li>list-22</li>
-        <li>list-23</li>
-        <li>list-24</li>
-        <li>list-25</li>
-        <li>list-26</li>
-        <li>list-27</li>
-        <li>list-28</li>
-        <li>list-29</li>
-        <li>list-30</li>
-        <li>list-31</li>
-        <li>list-32</li>
-        <li>list-33</li>
-        <li>list-34</li>
-        <li>list-35</li>
-        <li>list-36</li>
-        <li>list-37</li>
-        <li>list-38</li>
-        <li>list-39</li>
-        <li>list-40</li>
-      </ul>
+    <div className="home_container">
+      <div className="home_wrapper">
+        <p className="fs-48 m-0 title_home">Master Coding Skills for the Future</p>
+        <span className='desc fs-20'>
+          Join thousands of students learning to code with our expert-led
+          courses
+        </span>
+        <span className='course_btn fs-16'>Explore Courses</span>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
